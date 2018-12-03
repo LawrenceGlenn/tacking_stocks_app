00e66e09f800760ad65b02445aa6918b9daa7513
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2018-12-03 15:38:21 -0800

    altered code so that the stock chart woudl adjust to minute by minute or day by day as per the data incoming from the Dynamic chart function of the API

--- app/assets/javascripts/stock_d3.js
+++ app/assets/javascripts/stock_d3.js
@@ -1,25 +1,33 @@
 $('document').ready(function() {
-var chart = $('.stock_info').data('stock').chart;
+var data = $('.stock_info').data('stock').chart;
 //alert($('.stock_info').data('stock').chart[0]);
-drawChart(parseData(chart));
+drawStockChart(parseData(data));
 });
 
 function parseData(chart) {
-   var arr = [];
-   for (var i in chart) {
-      arr.push(
-         {
-            date: new Date(chart[i].date),  //convert string to number
-            close: +chart[i].close, //convert string to number
-            open: +chart[i].open, //convert string to number
-            low: +chart[i].low, //convert string to number
-            high: +chart[i].high //convert string to number
-         });
+  var arr = [];
+  //create a d3 parser to format the time
+  var myTimeParser = d3.timeParse("%Y-%m-%d");
+  var time = "";
+  for (var i in chart) {
+    if(chart[i].label.includes(" AM") || chart[i].label.includes(" PM")){
+      time = chart[i].label;
+      myTimeParser = (chart[i].label.includes(":"))? d3.timeParse("%Y-%m-%d %H:%M %p") : d3.timeParse("%Y-%m-%d %H %p");    
+    }
+    arr.push(
+      {
+        //combine time of day and date and parse it into d3 format
+        date: myTimeParser(chart[i].date+" "+time),
+        close: +chart[i].close, //convert string to number
+        open: +chart[i].open, //convert string to number
+        low: +chart[i].low, //convert string to number
+        high: +chart[i].high //convert string to number
+    });
    }
-   return arr;
+  return arr;
 }
 
-function drawChart(chart) {
+function drawStockChart(data) {
 
   //set dimensions for the canvis the graph will be on
   var svgWidth = 600, svgHeight = 400;
@@ -31,19 +39,20 @@ function drawChart(chart) {
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
+  // move the drawing area over by margin to make room for the axis
   var g = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//set the range of the graph
+  //set the range of the graph
   var x = d3.scaleTime().rangeRound([0, width]);
   var y = d3.scaleLinear().rangeRound([height, 0]);
 
-  x.domain(d3.extent(chart, function(d) { return d.date }));
+  x.domain(d3.extent(data, function(d) { return d.date }));
   y.domain(d3.extent(
-    [].concat(chart.map(function (d) {return d.open}))
-    .concat(chart.map(function (d) {return d.close}))
-    .concat(chart.map(function (d) {return d.low}))
-    .concat(chart.map(function (d) {return d.high}))
+    [].concat(data.map(function (d) {return d.open}))
+    .concat(data.map(function (d) {return d.close}))
+    .concat(data.map(function (d) {return d.low}))
+    .concat(data.map(function (d) {return d.high}))
     ));
 
   var closeLine = d3.line()
@@ -77,11 +86,12 @@ g.append("g")
     .call(d3.axisLeft(y))
     .append("text")
     .attr("fill", "#000")
-    .attr("text-anchor", "end")
+    .style("text-anchor", "end")
     .text("Price ($)");
 
   g.append("path")
-    .datum(chart)
+    .datum(data)
+    .attr("class", "line")
     .attr("fill", "none")
     .attr("stroke", "steelblue")
     .attr("stroke-linejoin", "round")
@@ -90,7 +100,8 @@ g.append("g")
     .attr("d", closeLine);
 
   g.append("path")
-    .datum(chart)
+    .datum(data)
+    .attr("class", "line")
     .attr("fill", "none")
     .attr("stroke", "gray")
     .attr("stroke-linejoin", "round")
@@ -99,7 +110,8 @@ g.append("g")
     .attr("d", openLine);
 
   g.append("path")
-    .datum(chart)
+    .datum(data)
+    .attr("class", "line")
     .attr("fill", "none")
     .attr("stroke", "red")
     .attr("stroke-linejoin", "round")
@@ -108,7 +120,8 @@ g.append("g")
     .attr("d", lowLine);
 
   g.append("path")
-    .datum(chart)
+    .datum(data)
+    .attr("class", "line")
     .attr("fill", "none")
     .attr("stroke", "green")
     .attr("stroke-linejoin", "round")
@@ -117,4 +130,57 @@ g.append("g")
     .attr("d", highLine);
 
 
+    //hover(svg, svg.selectAll(".line"), data);
+
+
   }
+
+function hover(svg, path, data) {
+  svg.style("position", "relative");
+  
+  if ("ontouchstart" in document) svg
+      .style("-webkit-tap-highlight-color", "transparent")
+      .on("touchmove", moved)
+      .on("touchstart", entered)
+      .on("touchend", left)
+  else svg
+      .on("mousemove", moved)
+      .on("mouseenter", entered)
+      .on("mouseleave", left);
+
+  const dot = svg.append("g")
+      .attr("display", "none");
+
+  dot.append("circle")
+      .attr("r", 2.5);
+
+  dot.append("text")
+      .style("font", "10px sans-serif")
+      .attr("text-anchor", "middle")
+      .attr("y", -8);
+
+  function moved() {
+    d3.event.preventDefault();
+    const ym = y.invert(d3.event.layerY);
+    const xm = x.invert(d3.event.layerX);
+    const i1 = d3.bisectLeft(data.dates, xm, 1);
+    const i0 = i1 - 1;
+    const i = xm - data.dates[i0] > data.dates[i1] - xm ? i1 : i0;
+    const s = data.series.reduce((a, b) => Math.abs(a.values[i] - ym) < Math.abs(b.values[i] - ym) ? a : b);
+    path.attr("stroke", d => d === s ? null : "#ddd").filter(d => d === s).raise();
+    dot.attr("transform", `translate(${x(data.dates[i])},${y(s.values[i])})`);
+    dot.select("text").text(s.name);
+
+  focus.select('text').text(formatCurrency(d.close));
+  }
+
+  function entered() {
+    path.attr("stroke", "#ddd");
+    dot.attr("display", null);
+  }
+
+  function left() {
+    path.attr("stroke", "green");
+    dot.attr("display", "none");
+  }
+}
