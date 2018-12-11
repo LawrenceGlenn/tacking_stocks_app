c28400cc45cc017b3034f31704ad36d268cbe7e5
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2018-12-10 17:56:12 -0800

    added latest headlines to show

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,6 +6,7 @@ class Stock
     @price = IEX::Resources::Price.get(sym)
     @chart = IEX::Resources::Chart.get(sym, 'dynamic')
     @company = IEX::Resources::Company.get(sym)
+    @news = IEX::Resources::News.get(sym)
   end
 
   def chart_size
@@ -14,6 +15,10 @@ class Stock
 
   def name
     @company.company_name
+  end
+
+  def headline
+    @news.first.headline
   end
 
 
